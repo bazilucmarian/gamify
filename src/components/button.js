949534a24ae7e23989516a07e367c4f78b40593b
Baziluc Marian <bazilucmarian98@yahoo.com>
2021-07-12 13:14:41 +0300

    added color props to the button

---
+++ src/components/button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Button({children, variant, color, size, ...otherProps}) {
+  return (
+    <button type="button" className={`btn btn--${size} ${color} ${variant}`} {...otherProps}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'fourth']),
+  variant: PropTypes.oneOf([
+    'outlined-primary',
+    'outlined-secondary',
+    'outlined-tertiary',
+    'outlined-fourth',
+    'contained-primary',
+    'contained-secondary',
+    'contained-tertiary',
+    'contained-fourth'
+  ]),
+  size: PropTypes.oneOf(['lg', 'md', 'sm'])
+};
+Button.defaultProps = {
+  variant: 'secondary',
+  color: 'outlined-secondary',
+  size: 'md'
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 import calculateXp from './calculateXp';
 import navLinks from './nav-links';
 import getInitials from './getInitials';
-import {variantOptions} from './variant-options';
 
-export {calculateXp, navLinks, getInitials, variantOptions};
+export {calculateXp, navLinks, getInitials};

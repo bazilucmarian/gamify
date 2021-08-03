8f979383d94a07640a0b5227fe7ef0e6de1fbde5
Baziluc Marian <bazilucmarian98@yahoo.com>
2021-08-03 17:25:41 +0300

    added edit shop-item functionality

---
+++ src/utils/form-rules-shop.js
@@ -0,0 +1,23 @@
+import {descriptionMinLength, maxCredits, minCredits, titleMinLength} from '../constants/form-validation';
+
+/* eslint-disable prefer-const */
+export default function validateShopForm(fields) {
+  const specialCharacters = /[!"#$%*<>?^~§©®°¶]+/;
+
+  let errors = {};
+  if (fields.title.trim().length < titleMinLength) {
+    errors.title = `Please lengthen title to ${titleMinLength} characters or more`;
+  } else if (fields.title.match(specialCharacters) !== null) {
+    errors.title = 'Remove special characters from the title.';
+  }
+
+  if (!(fields.credits >= minCredits && fields.credits <= maxCredits)) {
+    errors.credits = `Credits must be between ${minCredits} and ${maxCredits}`;
+  }
+
+  if (fields.description.trim().length < descriptionMinLength) {
+    errors.description = `Please lengthen description to ${descriptionMinLength} characters or more`;
+  }
+
+  return errors;
+}

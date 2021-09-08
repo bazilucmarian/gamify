afff3933ee5566fd6bc609937247b8a6a6a41c5c
Baziluc Marian <bazilucmarian98@yahoo.com>
2021-09-08 10:39:12 +0300

    commit again

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use("/api/cart", shoppingCartRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use("*", express.static(path.join(__dirname, "gamify/build")));
+  app.use(xpress.static(path.join(__dirname, "gamify/build")));
   app.get("*", (_, res) =>
     res.sendFile(path.resolve(__dirname, "gamify", "build", "index.html"))
   );

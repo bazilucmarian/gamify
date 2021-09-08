67cc9a1d7921e36456d3a8d34f020f642eb62f9c
Baziluc Marian <bazilucmarian98@yahoo.com>
2021-09-08 16:02:20 +0300

    change path to gamify

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "gamify/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./gamify/build/index.html"));
+    res.sendFile(path.join(__dirname, "../gamify/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
